03fc8d09d99d3bae16673dfa943a7b9def866e2e
gingfrederik <jeff26912317@gmail.com>
2017-06-23 10:53:35 +0800

    docs add

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,6 +1,27 @@
 <template>
   <main class="container"> 
-  <br></br>
+  <br><button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+    Launch demo modal
+  </button></br>
+  <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <div class="modal-body">
+          <p>fuck</p>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+          <button type="button" class="btn btn-primary">Save changes</button>
+        </div>
+      </div>
+    </div>
+  </div>
   <table id="example" class="table table-striped table-bordered nowrap" cellspacing="0" width="100%">
     <thead>
       <tr>
